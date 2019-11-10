513ef5e8c8aa6ed69d8ca5c9b7926b41fb43c628
Kshamina <kshamina.ghelani@carleton.ca>
2019-11-10 14:38:37 -0500

    Updates

---
+++ precache-manifest.c1e751d43b467d3ea54cb7f632cb651d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70dc9e5f4e781142262c8f9e73021c3",
+    "url": "/website-react/index.html"
+  },
+  {
+    "revision": "592213d1e9b57495738a",
+    "url": "/website-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c0d9ae7dd54cc142822b",
+    "url": "/website-react/static/js/2.62622b70.chunk.js"
+  },
+  {
+    "revision": "592213d1e9b57495738a",
+    "url": "/website-react/static/js/main.455c0a3f.chunk.js"
+  },
+  {
+    "revision": "57baea17db1b83d27029",
+    "url": "/website-react/static/js/runtime-main.f8845bb3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website-react/static/media/logo.25bf045c.svg"
+  }
+]);
